e009af15397a42e32c3ef9cb89436a1227ef3462
_Finder丶Tiwk <136652711@qq.com>
2016-08-01 23:54:39 +0800

    #Fix: 修复文件模板问题

--- InterfaceBuilderKit/Src/UIButton/IBPlaceHolderButton.m
+++ InterfaceBuilderKit/Src/UIButton/IBPlaceHolderButton.m
@@ -2,6 +2,12 @@
 //  IBPlaceHolderButton.m
 //  InterfaceBuilderKit
 //
+//  _____  _           _          _____ _          _
+//  |  ___(_)_ __   __| | ___ _ _|_   _(_)_      _| | __
+//  | |_  | | '_ \ / _` |/ _ \ '__|| | | \ \ /\ / / |/ /
+//  |  _| | | | | | (_| |  __/ |   | | | |\ V  V /|   <
+//  |_|   |_|_| |_|\__,_|\___|_|   |_| |_| \_/\_/ |_|\_\
+//
 //  Created by _Finder丶Tiwk on 16/8/1.
 //  Copyright © 2016年 _Finder丶Tiwk. All rights reserved.
 //
